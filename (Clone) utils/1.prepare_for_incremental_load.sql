badca506e02ffb8c9315a6677918c0251c0fd149
sonalkarale <sonalkarale>
2024-04-13 05:31:56 +0000

    ASA

---
+++ (Clone) utils/1.prepare_for_incremental_load.sql
@@ -0,0 +1,23 @@
+-- Databricks notebook source
+-- MAGIC %md 
+-- MAGIC ### Drop all the tables 
+
+-- COMMAND ----------
+
+drop database if exists f1_processed cascade;
+
+-- COMMAND ----------
+
+create database if not exists f1_processed
+location "/mnt/formula1datalake24/processed"
+
+-- COMMAND ----------
+
+drop database if exists f1_presentation cascade;
+
+
+
+-- COMMAND ----------
+
+create database if not exists f1_presentation
+location "/mnt/formula1datalake24/presentation"
